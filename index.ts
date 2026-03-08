d60619d03fd2ad958b23d8c1aca3b217821e3639
endgula <endgula20@gmail.com>
2026-03-08 02:41:27 -0500

    Conf

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 const server = Bun.serve({
+  hostname: "0.0.0.0", // Esto es vital para que Coolify lo detecte
   port: process.env.PORT ?? 3000,
   async fetch(req) {
-    return new Response("API de Bun está funcionando correctamente");
-  }
-})
+    return new Response("API de Bun activa y funcionando en Coolify");
+  },
+});
 
-console.log(`Server is running on ${server.url}:${server.port}`);
+console.log(`Servidor activo en ${server.hostname}:${server.port}`);
